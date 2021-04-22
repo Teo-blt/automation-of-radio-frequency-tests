9bf72d07095aabe9657622db166f16d3c8f02257
Teo-blt <82824044+Teo-blt@users.noreply.github.com>
2021-04-22 10:28:59 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# =============================================================================
+# Created By  : Bulteau Téo
+# Created Date: April 18 16:00:00 2021
+# For Kerlik, all rights reserved
+# =============================================================================
+"""The Module Has Been Build for the automation of radio frequency tests in python language"""
+# =============================================================================
+# Imports
+# Personal Imports
+import Menu as me
+# =============================================================================
+
+me.Application().mainloop()
+
