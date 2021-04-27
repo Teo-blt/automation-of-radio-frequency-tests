864ef1949233e6c684406cd8edbab2c01847e20b
teo_blt <teo.bulteau@gmail.com>
2021-04-27 16:54:27 +0200

    test

--- Climate chamber control/Climate_chamber.py
+++ Climate chamber control/Climate_chamber.py
@@ -7,15 +7,7 @@
 ################################################
 ##### Cycle SETTINGS
 ################################################
-temp_min = 0
-temp_min_duration_h = 1
 
-temp_max = 20
-temp_max_duration_h = 1
-
-change_min_duration_h = 1
-
-nb_cycle = 20
 
 # import visa
 import serial  # requirment pyserial
@@ -23,109 +15,123 @@ import os
 import time
 import sys
 
-# sys.path.append('\\\\samba\\share\\projet\\e2b\\hardware\\Scripts_auto\\Python\\lib')
 
-################################################
-##### VISA instrument
-serial_speed = 9600
-serial_timeout = 5
-VT = serial.Serial('COM11', serial_speed, timeout=serial_timeout)
+def init(temp_min):
+    try:
+        # sys.path.append('\\\\samba\\share\\projet\\e2b\\hardware\\Scripts_auto\\Python\\lib')
 
-################################################################################################
-## MEASUREMENT Loop
-################################################################################################
-print("\n################################################\n")
-print("\nStart of Test\n")
-time_start = time.time()
+        ################################################
+        ##### VISA instrument
+        serial_speed = 9600
+        serial_timeout = 5
+        VT = serial.Serial('COM11', serial_speed, timeout=serial_timeout)
 
-# ToDo : Display cycles carac
+        ################################################################################################
+        ## MEASUREMENT Loop
+        ################################################################################################
+        print("\n################################################\n")
+        print("\nStart of Test\n")
+        time_start = time.time()
 
-for i in range(0, nb_cycle):
-    VT.write(b"$00I\n\r")
-    time.sleep(0.5)
-    received_frame = VT.read_all().decode('utf-8')
-    print(received_frame)
-    print("\n")
-    # ToDo print actual temp
-    # ToDo Start Log
+        # ToDo : Display cycles carac
 
-    print(f'Start cycle {i} low temp:\n')
+        temp_min_duration_h = 1
 
-    # Set Temp Min
-    VT.write(b"$00E %06.1f 0000.0 0000.0 0000.0 0000.0 0101000000000000\n\r" % temp_min)
-    time.sleep(2)
+        temp_max = 20
+        temp_max_duration_h = 1
 
-    # Wait temperature stabilisation
-    change_cycle_start_time = time.time()
-    while time.time() <= change_cycle_start_time + change_min_duration_h * 3600:
-        # Read temp every 5 min
-        VT.write(b"$00I\n\r")
-        received_frame = VT.read_all().decode('utf-8')
-        print(received_frame)
-        print("\n")
-        time.sleep(5 * 60)
+        change_min_duration_h = 1
 
-    low_cycle_start_time = time.time()
-    while time.time() < low_cycle_start_time + (temp_min_duration_h * 3600):
-        # Read temp every 5 min
-        VT.write(b"$00I\n\r")
-        received_frame = VT.read_all().decode('utf-8')
-        print(received_frame)
-        print("\n")
-        time.sleep(5 * 60)
+        nb_cycle = 20
 
-    print(f'Start cycle {i} High temp:\n')
-    # Set Temp Max
-    VT.write(b"$00E %06.1f 0000.0 0000.0 0000.0 0000.0 0101000000000000\n\r" % temp_max)
-    time.sleep(2)
+        for i in range(0, nb_cycle):
+            VT.write(b"$00I\n\r")
+            time.sleep(0.5)
+            received_frame = VT.read_all().decode('utf-8')
+            print(received_frame)
+            print("\n")
+            # ToDo print actual temp
+            # ToDo Start Log
 
-    change_cycle_start_time = time.time()
-    while time.time() < change_cycle_start_time + (change_min_duration_h * 3600):
-        # Read temp every 5 min
-        VT.write(b"$00I\n\r")
-        received_frame = VT.read_all().decode('utf-8')
-        print(received_frame)
-        print("\n")
-        time.sleep(5 * 60)
+            print(f'Start cycle {i} low temp:\n')
 
-    high_cycle_start_time = time.time()
-    while time.time() < high_cycle_start_time + (temp_max_duration_h * 3600):
-        # Read temp every 5 min
-        VT.write(b"$00I\n\r")
-        received_frame = VT.read_all().decode('utf-8')
-        print(received_frame)
-        print("\n")
-        time.sleep(5 * 60)
+            # Set Temp Min
+            VT.write(b"$00E %06.1f 0000.0 0000.0 0000.0 0000.0 0101000000000000\n\r" % temp_min)
+            time.sleep(2)
 
-    print(f'End of cycle {i}: {time.time() - time_start}\n')
+            # Wait temperature stabilisation
+            change_cycle_start_time = time.time()
+            while time.time() <= change_cycle_start_time + change_min_duration_h * 3600:
+                # Read temp every 5 min
+                VT.write(b"$00I\n\r")
+                received_frame = VT.read_all().decode('utf-8')
+                print(received_frame)
+                print("\n")
+                time.sleep(5 * 60)
 
-# Stop climatic chamber
-VT.write(b"$00E 0020.0 0000.0 0000.0 0000.0 0000.0 0000000000000000\n\r")
+            low_cycle_start_time = time.time()
+            while time.time() < low_cycle_start_time + (temp_min_duration_h * 3600):
+                # Read temp every 5 min
+                VT.write(b"$00I\n\r")
+                received_frame = VT.read_all().decode('utf-8')
+                print(received_frame)
+                print("\n")
+                time.sleep(5 * 60)
 
-time_stop = time.time()
-print("\n################################################\n")
-print("\nEnd of Test\n")
-print(f'Test duration: {time_stop - time_start}\n')
-VT.close()
+            print(f'Start cycle {i} High temp:\n')
+            # Set Temp Max
+            VT.write(b"$00E %06.1f 0000.0 0000.0 0000.0 0000.0 0101000000000000\n\r" % temp_max)
+            time.sleep(2)
 
-"""# Votsch climatic chamber VT4002
+            change_cycle_start_time = time.time()
+            while time.time() < change_cycle_start_time + (change_min_duration_h * 3600):
+                # Read temp every 5 min
+                VT.write(b"$00I\n\r")
+                received_frame = VT.read_all().decode('utf-8')
+                print(received_frame)
+                print("\n")
+                time.sleep(5 * 60)
 
-# Set temperature +80 & mise à ON
-$00E 0080.0 0000.0 0000.0 0000.0 0000.0 0101000000000000
+            high_cycle_start_time = time.time()
+            while time.time() < high_cycle_start_time + (temp_max_duration_h * 3600):
+                # Read temp every 5 min
+                VT.write(b"$00I\n\r")
+                received_frame = VT.read_all().decode('utf-8')
+                print(received_frame)
+                print("\n")
+                time.sleep(5 * 60)
 
-# Set temperature -40 & mise à ON
-$00E -040.0 0000.0 0000.0 0000.0 0000.0 0101000000000000
-$00E -040.0 0000.0 0000.0 0000.0 0000.0 0101000000000000
+            print(f'End of cycle {i}: {time.time() - time_start}\n')
 
-# Off
-$00E 0020.0 0000.0 0000.0 0000.0 0000.0 0000000000000000
+        # Stop climatic chamber
+        VT.write(b"$00E 0020.0 0000.0 0000.0 0000.0 0000.0 0000000000000000\n\r")
 
+        time_stop = time.time()
+        print("\n################################################\n")
+        print("\nEnd of Test\n")
+        print(f'Test duration: {time_stop - time_start}\n')
+        VT.close()
 
-
-# Ask temperature & ref
-$00I
-
-
-> Answer
-0085.0 -039.9 0000.0 -000.1 0000.0 -190.3 0000.0 -190.3 0000.0 -190.3 0101000000000000
-"""
+        """# Votsch climatic chamber VT4002
+        
+        # Set temperature +80 & mise à ON
+        $00E 0080.0 0000.0 0000.0 0000.0 0000.0 0101000000000000
+        
+        # Set temperature -40 & mise à ON
+        $00E -040.0 0000.0 0000.0 0000.0 0000.0 0101000000000000
+        $00E -040.0 0000.0 0000.0 0000.0 0000.0 0101000000000000
+        
+        # Off
+        $00E 0020.0 0000.0 0000.0 0000.0 0000.0 0000000000000000
+        
+        
+        
+        # Ask temperature & ref
+        $00I
+        
+        
+        > Answer
+        0085.0 -039.9 0000.0 -000.1 0000.0 -190.3 0000.0 -190.3 0000.0 -190.3 0101000000000000
+        """
+    except:
+        print("error")
--- Climate chamber control/Menu_climate.py
+++ Climate chamber control/Menu_climate.py
@@ -14,27 +14,53 @@ import tkinter as tk
 import sys
 from tkinter import *
 from tkinter import ttk
+import Climate_chamber
+
+
 # =============================================================================
 
 class Climate(Tk):
     def __init__(self):
         Tk.__init__(self)
+        global elcolor
+        elcolor = "#E76145"
         self.settings_chamber()
 
     def settings_chamber(self):  # creation of a lobby menu
-        my_settings_chamber_frame = LabelFrame(self, text="Choice of instrument",bg="grey")
+        b = IntVar()
+        my_settings_chamber_frame = LabelFrame(self, text="Choice of instrument")
         my_settings_chamber_frame.grid(row=0, column=0, ipadx=40, ipady=40, padx=0, pady=0)
-        label = tk.Label(my_settings_chamber_frame, text="Settings_chamber", )
-        label.pack(padx=10, pady=10, expand=True, fill="both", side=TOP)
+        label = tk.Label(my_settings_chamber_frame, text="Settings_chamber", bg="white", font="arial",
+                         fg="black", relief="groove")
+        label.grid(row=0, column=0, ipadx=40, ipady=40, padx=0, pady=0)
         button1 = tk.Button(my_settings_chamber_frame, text="Start",
-                            borderwidth=8, background="red",
+                            borderwidth=8, background=elcolor,
                             activebackground="green", cursor="right_ptr", overrelief="sunken",
-                            command=lambda: [Climate chamber])
-        button1.pack(padx=30, pady=10, expand=True, fill="both", side=TOP)
+                            command=lambda: [Climate_chamber.init(scale1.get())])
+        button1.grid(row=0, column=1, ipadx=40, ipady=20, padx=0, pady=0)
         button2 = tk.Button(my_settings_chamber_frame, text="Quit",
-                            borderwidth=8, background="red",
+                            borderwidth=8, background=elcolor,
                             activebackground="green", cursor="right_ptr", overrelief="sunken",
                             command=lambda: [sys.exit()])
-        button2.pack(padx=30, pady=10, expand=True, fill="both", side=TOP)
+        button2.grid(row=0, column=2, ipadx=40, ipady=20, padx=0, pady=0)
+        scale1 = Scale(my_settings_chamber_frame, orient='vertical', troughcolor=elcolor, from_=100, to=-20,
+                       resolution=1, tickinterval=20, length=100, command=0,
+                       label='temperature_min', state="active")
+        scale1.grid(row=1, column=0, ipadx=40, ipady=40, padx=0, pady=0)
+        scale2 = Scale(my_settings_chamber_frame, orient='vertical', troughcolor=elcolor, from_=100, to=-20,
+                       resolution=1, tickinterval=20, length=100, command=0,
+                       label='temperature_max', state="active")
+        scale2.grid(row=2, column=0, ipadx=40, ipady=40, padx=0, pady=0)
+        RB1 = tk.Radiobutton(my_settings_chamber_frame, text="mono_cycle",
+                             variable=b, value=0, cursor="right_ptr", command=lambda: [scale3.configure(state="disabled")])
+        RB1.grid(row=1, column=1, ipadx=40, ipady=40, padx=0, pady=0)
+        RB2 = tk.Radiobutton(my_settings_chamber_frame, text="multi_cycles",
+                             variable=b, value=1, cursor="right_ptr", command=lambda: [scale3.configure(state="active")])
+        RB2.grid(row=1, column=2, ipadx=40, ipady=40, padx=0, pady=0)
+        scale3 = Scale(my_settings_chamber_frame, orient='horizontal', troughcolor=elcolor, from_=0, to=20,
+                       resolution=1, tickinterval=5, length=100, command=0,
+                       label='nomber of cycles', state="disabled", relief="flat")
+        scale3.grid(row=2, column=1, ipadx=40, ipady=40, padx=0, pady=0)
+
 
 Climate().mainloop()
