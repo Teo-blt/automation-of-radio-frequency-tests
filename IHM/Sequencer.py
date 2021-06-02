5405055438c4608ca9968f46af688301687f8024
teo_blt <teo.bulteau@gmail.com>
2021-06-02 12:10:53 +0200

    sequenceur menu

--- IHM/Sequencer.py
+++ IHM/Sequencer.py
@@ -9,10 +9,22 @@
 # =============================================================================
 import tkinter as tk
 from tkinter import *
+import tkinter
+import tkinter.messagebox
+import datetime
 
 
 # =============================================================================
 def sequencer(self):
+
+    def showday():
+        now = datetime.datetime.now()
+        msg = 'Today is: {}'.format(now.strftime('%A'))
+        tkinter.messagebox.showinfo("Information", msg)
+
+    def showmenu(e):
+        pmenu.post(e.x_root, e.y_root)
+
     sequencer_frame = LabelFrame(self)
     sequencer_frame.grid(row=0, column=2, ipadx=40, ipady=40, padx=0, pady=0, rowspan=4)
     sequencer_label = tk.Label(sequencer_frame, text="Sequencer frame", bg="white", font="arial",
@@ -20,3 +32,8 @@ def sequencer(self):
     sequencer_label.pack(padx=1, pady=1, expand=True, fill="both", side=TOP)
     sequencer_label_2 = tk.Label(sequencer_frame, text="in progress...")
     sequencer_label_2.pack(padx=1, pady=1, expand=True, fill="both", side=TOP)
+
+    pmenu = tkinter.Menu(self, tearoff=0)
+    pmenu.add_command(label="Show day", command=showday)
+    pmenu.add_command(label="Exit", command=self.quit)
+    self.bind("<Button-3>", showmenu)
