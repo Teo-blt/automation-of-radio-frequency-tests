f51cf23839b7c617c0a2e5f1a32176e00c6e693e
teo_blt <teo.bulteau@gmail.com>
2021-06-30 10:31:01 +0200

    test graphique tableau channel ok

--- IHM/Graph_sensibility.py
+++ IHM/Graph_sensibility.py
@@ -7,34 +7,28 @@ def draw_graph():
     try:
         data = pd.read_csv('test.txt', sep='\s+', header=None)
         data = pd.DataFrame(data)
-        x = []
-        y = []
-        x1 = []
-        y1 = []
-        x2 = []
-        y2 = []
-        x3 = []
-        y3 = []
-        for i in range(0, len(data)):
-            if data[2][i] == 0:
-                if (data[3][i]) == 0:
-                    x = x + [data[0][i]]
-                    y = y + [data[1][i]]
-                else:
-                    x1 = x1 + [data[0][i]]
-                    y1 = y1 + [data[1][i]]
-            else:
-                if data[3][i] == 0:
-                    x2 = x2 + [data[0][i]]
-                    y2 = y2 + [data[1][i]]
-                else:
-                    x3 = x3 + [data[0][i]]
-                    y3 = y3 + [data[1][i]]
 
-        plt.plot(x, y, 'b', marker=",")
-        plt.plot(x1, y1, 'r', marker=",")
-        plt.plot(x2, y2, 'y', marker=",")
-        plt.plot(x3, y3, 'g', marker=",")
+        x = {}
+        y = {}
+        i = 0
+
+        while i != len(data):
+            if i != 0:
+                i = i + 1
+            x[data[3][i]] = ([data[0][i]])
+            y[data[3][i]] = ([data[1][i]])
+            try:
+                while data[3][i] == data[3][i + 1]:
+                    x[data[3][i]] = x[data[3][i]] + [data[0][i + 1]]
+                    y[data[3][i]] = y[data[3][i]] + [data[1][i + 1]]
+                    i = i + 1
+            except:
+                break
+
+        print('fini')
+        color = {0: 'b', 1: 'r', 2: 'g', 3: 'y', 4: 'c', 5: 'lime', 6: 'black', 7: 'pink'}
+        for m in range(0, 8):
+            plt.plot(x[m], y[m], color[m], marker=",")
 
         plt.xlabel("Power at the entrance of the receiver in dBm")
         plt.ylabel("% of packet lost")
